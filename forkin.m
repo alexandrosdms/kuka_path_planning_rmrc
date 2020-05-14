626aee6be2c5f11bcc0f5e421e41ce5b43e8e9b6
Alexandros Dimas <alexandrosdms98@gmail.com>
2020-05-14 14:46:04 +0300

    solutions of inverse  kinematics ar shown on a table

--- forkin.m
+++ forkin.m
@@ -17,28 +17,32 @@ function [] = forkin();
     M_tot = zeros(4,16);    % All matrices will be stored here
     for i = index  
         M = [cos(th(i)) -sin(th(i)) 0 l(i);
-            sin(th(i))*cos(a(i)) cos(th(i))*cos(a(i)) -sin(a(i)) -sin(a(i))*d(i);
-            sin(th(i))*sin(a(i)) cos(th(i))*sin(a(i)) cos(a(i)) cos(a(i))*d(i);
+            sin(th(i))*cos(a(i)) cos(th(i))*cos(a(i)) ...
+            -sin(a(i)) -sin(a(i))*d(i);
+            sin(th(i))*sin(a(i)) cos(th(i))*sin(a(i)) cos(a(i)) ...
+            cos(a(i))*d(i);
             0 0 0 1];
 
         M_tot(1:4,m:m+3) = M;
         m = m+4;
     end
 
-    M10 = M_tot(:,1:4)      % 1 to 0 transformation
-    M21 = M_tot(:,5:8)      % 2 to 1    >>
-    M32 = M_tot(:,9:12)     % 3 to 2    >>
-    M43 = M_tot(:,13:16)    % 4 to 3    >>
-    M54 = M_tot(:,17:20)    % 5 to 4    >>
-    M65 = M_tot(:,21:24)    % 6 to 5    >>
+    M10 = M_tot(:,1:4);      % 1 to 0 transformation
+    M21 = M_tot(:,5:8);      % 2 to 1    >>
+    M32 = M_tot(:,9:12);     % 3 to 2    >>
+    M43 = M_tot(:,13:16);    % 4 to 3    >>
+    M54 = M_tot(:,17:20);    % 5 to 4    >>
+    M65 = M_tot(:,21:24);    % 6 to 5    >>
 
     M60 = M65*M54*M43*M32*M21*M10;  % tip to base transformation matrix
-
+    
+    disp('TCP in meters')
     p = M60(1:3,4)  % Position of arm's tip in meters
 
     % Euler ZYX angles are used to describe tip's orientation
     % all angles are displayed in radians
-    fi = atan(M60(2,1)/M60(1,1))    % Euler Z angle 
-    theta = atan(-M60(3,1)/sqrt((M60(1,1)^2 + M60(2,1)^2))) % Euler Y angle
-    psi_ = atan(M60(2,3)/M60(3,3))   % Euler X angle
+    disp('Euler ZYX angles')
+    fz = atan(M60(2,1)/M60(1,1))    % Euler Z angle 
+    fy = atan(-M60(3,1)/sqrt((M60(1,1)^2 + M60(2,1)^2))) % Euler Y angle
+    fx_ = atan(M60(2,3)/M60(3,3))   % Euler X angle
 end
--- invkin.m
+++ invkin.m
@@ -1,123 +1,157 @@
-%function [th1, th2, th3, th4, th5, th6] = invkin()
-% prompt = "Input TCP's position in vector form: "
-% p40 = input(prompt)
-% 
-% prompt = "Input Euler ZYX angles for TCP position in vector form: "
-% angles = pi/180 * input(prompt)
-clc
-clear all
-p40 = [0.1 0.1 0.05];
-angles = pi/180 * [39 52 78];
+function sol_tot = invkin()
+    prompt = "Input TCP (in meters) in vector form [px py pz]: "
+    p40_0 = input(prompt)
 
-fi = angles(1);
-theta = angles(2);
-psi_ = angles(3);
+    prompt = "Input EulerZYX [fz fy fx]: "
+    angles = pi/180 * input(prompt)
+%     clc
+%     clear all
+%     p40_0 = [0.1 0.1 0.05];       %testing values
+%     angles = pi/180 * [39 52 78]; %testing values
+    
+    fi = angles(1);
+    theta = angles(2);
+    psi_ = angles(3);
 
-R_ZYX = [cos(fi)*cos(theta) (cos(fi)*sin(theta)*sin(psi_))-(sin(fi)*cos(psi_)) (cos(fi)*sin(theta)*cos(psi_))+(sin(fi)*sin(psi_));
-    sin(fi)*cos(theta) (sin(fi)*sin(theta)*sin(psi_))+(cos(fi)*cos(psi_)) (sin(fi)*sin(theta)*cos(psi_))-(cos(fi)*sin(psi_)) ;
-    -sin(theta) cos(theta)*sin(psi_) cos(theta)*cos(psi_)];
-R60 = R_ZYX;
+    R_ZYX = [cos(fi)*cos(theta)...
+        (cos(fi)*sin(theta)*sin(psi_))-(sin(fi)*cos(psi_)) ...
+        (cos(fi)*sin(theta)*cos(psi_))+(sin(fi)*sin(psi_));
+        sin(fi)*cos(theta) ...
+        (sin(fi)*sin(theta)*sin(psi_))+(cos(fi)*cos(psi_)) ...
+        (sin(fi)*sin(theta)*cos(psi_))-(cos(fi)*sin(psi_));
+        -sin(theta) cos(theta)*sin(psi_) cos(theta)*cos(psi_)];
+    R60 = R_ZYX;
 
-p1 = p40(1);
-p2 = p40(2);
-p3 = p40(3);
-%th1
-th1 = [atan(p2/p1) atan(p2/p1) + pi]
-%th3
-A = -1 - 6.519*((p1./cos(th1) - 0.15).^2 + (p3 - 0.45).^2 - 0.7837);
-B = 9.954;
-C = 1 - 6.519*((p1./cos(th1) - 0.15).^2 + (p3 - 0.45).^2 - 0.7837);
+    p1 = p40_0(1);
+    p2 = p40_0(2);
+    p3 = p40_0(3);
+    %th1
+    th1 = [atan(p2/p1) atan(p2/p1) + pi];
+    %th3
+    A = -1 - 6.519*((p1./cos(th1) - 0.15).^2 + (p3 - 0.45).^2 - 0.7837);
+    B = 9.954;
+    C = 1 - 6.519*((p1./cos(th1) - 0.15).^2 + (p3 - 0.45).^2 - 0.7837);
 
-P = [A(1) B C(1)];
-th3_1 = roots(P);
+    P = [A(1) B C(1)];
+    th3_1 = roots(P);
 
-P = [A(2) B C(2)];
-th3_2 = roots(P);
+    P = [A(2) B C(2)];
+    th3_2 = roots(P);
 
-th3 = [th3_1' th3_2']
+    th3 = [th3_1' th3_2'];
 
-%th2
-syms x y
-a11 = 0.59 + 0.13*cos(th3) + 0.64707*sin(th3);
-a12 = 0.13*sin(th3) - 0.64707*cos(th3);
-a21 = 0.64707*cos(th3) - 0.13*sin(th3);
-a22 = 0.59 + 0.13*cos(th3) + 0.64707*sin(th3);
+    %th2
+    syms x y
+    a11 = 0.59 + 0.13*cos(th3) + 0.64707*sin(th3);
+    a12 = 0.13*sin(th3) - 0.64707*cos(th3);
+    a21 = 0.64707*cos(th3) - 0.13*sin(th3);
+    a22 = 0.59 + 0.13*cos(th3) + 0.64707*sin(th3);
 
-b1 = p3 - 0.45;
-b2 = p1./cos(th1) - 0.15;
+    b1 = p3 - 0.45;
+    b2 = p1./cos(th1) - 0.15;
 
-eq11 = a11(1)*x + a12(1)*y == b1;
-eq21 = a21(1)*x + a22(1)*y == b2(1);
+    s2_0 = [];
+    c2_0 = [];
+    for i = 1:4
+        for j = 1:2
+            eq1 = a11(i)*x + a12(i)*y == b1;
+            eq2 = a21(i)*x + a22(i)*y == b2(j);
 
-eq12 = a11(1)*x + a12(1)*y == b1;
-eq22 = a21(1)*x + a22(1)*y == b2(2);
-
-eq13 = a11(2)*x + a12(2)*y == b1;
-eq23 = a21(2)*x + a22(2)*y == b2(1);
-
-eq14 = a11(2)*x + a12(2)*y == b1;
-eq24 = a21(2)*x + a22(2)*y == b2(2);
-
-eq15 = a11(3)*x + a12(3)*y == b1;
-eq25 = a21(3)*x + a22(3)*y == b2(1);
-
-eq16 = a11(3)*x + a12(3)*y == b1;
-eq26 = a21(3)*x + a22(3)*y == b2(2);
-
-eq17 = a11(4)*x + a12(4)*y == b1;
-eq27 = a21(4)*x + a22(4)*y == b2(1);
-
-eq18 = a11(4)*x + a12(4)*y == b1;
-eq28 = a21(4)*x + a22(4)*y == b2(2);
-
-sol1 = solve([eq11, eq21], [x, y]);
-sol2 = solve([eq12, eq22], [x, y]);
-sol3 = solve([eq13, eq23], [x, y]);
-sol4 = solve([eq14, eq24], [x, y]);
-sol5 = solve([eq15, eq25], [x, y]);
-sol6 = solve([eq16, eq26], [x, y]);
-sol7 = solve([eq17, eq27], [x, y]);
-sol8 = solve([eq18, eq28], [x, y]);
-
-s2 = [sol1.x sol2.x sol3.x sol4.x sol5.x sol6.x sol7.x sol8.x];
-c2 = [sol1.y sol2.y sol3.y sol4.y sol5.y sol6.y sol7.y sol8.y];
-s2 = double(s2);
-c2 = double(c2);
-
-t2 = s2./c2;
-th2 = atan(t2)
-
-th4 = 0;
-th5 = 0;
-th3 = 0;
-
-k = 1
-for i = th3
-    for j = th1
-        R30 = calcr30(i, j, th2(k));
-        R63 = R30'*R60;
-        
-        th4_1 = atan(R63(3,3)/R63(1,3));
-        th4_2 = th4_1 + pi;
-        th4 = [th4 th4_1  th4_2]
-
-        cos_5 = R63(2,3);
-        sin_5 = sqrt(1-cos_5^2);
-        th5_1 = atan(sin_5/cos_5);
-        th5_2 = th5_1 + pi;
-        th5 = [th5 th5_1 th5_2];
-
-        th6_1 = atan(-R63(2,2)/R63(2,1));
-        th6_2 = th6_1 + pi;
-        th3 = [th3 th6_1 th6_2];
-        k = k+1;
+            sol = solve([eq1 eq2], [x y]);
+            s2 = sol.x;
+            s2_0 = [s2_0 s2];
+            c2 = sol.y;
+            c2_0 = [c2_0 c2];
+        end
     end
-end
 
-th1 = 180/pi * th1(1,2:17)
-th2 = 180/pi * th2(1,2:17)
-th3 = 180/pi * th3(1,2:17)
-th4 = 180/pi * th4(1,2:17)
-th5 = 180/pi * th5(1,2:17)
-th6 = 180/pi * th6(1,2:17)
-%end
+    s2 = double(s2_0);
+    c2 = double(c2_0);
+    t2 = s2./c2;
+    th2 = atan(t2);
+
+    th4 = 0;
+    th5 = 0;
+    th6 = 0;
+
+    k = 1;
+    for i = th3
+        for j = th1
+            R30 = calcr30(i, j, th2(k));
+            R63 = R30'*R60;
+
+            th4_1 = atan(R63(3,3)/R63(1,3));
+            th4_2 = th4_1 + pi;
+            th4 = [th4 th4_1  th4_2];
+
+            cos_5 = R63(2,3);
+            sin_5 = sqrt(1-cos_5^2);
+            th5_1 = atan(sin_5/cos_5);
+            th5_2 = th5_1 + pi;
+            th5 = [th5 th5_1 th5_2];
+
+            th6_1 = atan(-R63(2,2)/R63(2,1));
+            th6_2 = th6_1 + pi;
+            th6 = [th6 th6_1 th6_2];
+            k = k+1;
+        end
+    end
+
+    th1 = 180/pi * th1;
+    th2 = 180/pi * th2;
+    th3 = 180/pi * th3;
+    th4 = 180/pi * th4(1,2:17);
+    th5 = 180/pi * th5(1,2:17);
+    th6 = 180/pi * th6(1,2:17);
+
+    k = 1;
+    int = 0;
+    r = 1;
+    sol_tot = [sym('i') sym('th1') sym('th2') sym('th3') sym('th4') ...
+        sym('th5') sym('th6')];
+    
+    disp('Joint angles')
+    for i =  th3
+        if k<8
+            for j = th1
+                if r == 1
+                    k = 0;
+                    for m = k+1:k+2
+                        for l = k+1:k+2
+                            for p = k+1:k+2
+                                sol = [sym(num2str(i)) ...
+                                    sym(num2str(th2(k+1))) ...
+                                    sym(num2str(j)) ...
+                                    sym(num2str(th4(m))) ...
+                                    sym(num2str(th5(l))) ...
+                                    sym(num2str(th6(p)))];
+                                int = int + 1;
+                                sol_tot = [sol_tot; sym(num2str(int)) sol];
+                            end
+                        end
+                    end
+                else
+                    for m = k+1:k+2
+                        for l = k+1:k+2
+                            for p = k+1:k+2
+                                sol = [sym(num2str(i)) ...
+                                    sym(num2str(th2(k+1))) ...
+                                    sym(num2str(j)) ...
+                                    sym(num2str(th4(m))) ...
+                                    sym(num2str(th5(l))) ...
+                                    sym(num2str(th6(p)))];
+                                int = int + 1;
+                                sol_tot = [sol_tot; sym(num2str(int)) sol];
+                            end
+                        end
+                    end
+                end
+                r = r+1;
+                k = k+1;
+            end
+        end
+    end
+
+    int = int; % must be 64
+    disp('Joint angles')
+end
