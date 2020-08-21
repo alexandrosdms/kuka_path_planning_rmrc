36faf3cbb6f881fc535345772b894fcb6b7cd935
Alexandros Dimas <alexandrosdms98@gmail.com>
2020-08-21 17:24:56 +0300

    Full (8) solutions 'sols' matrix added

--- invkin.m
+++ invkin.m
@@ -9,6 +9,7 @@ function [theta1,theta2,theta3,theta4,theta5,theta6] = invkin(Px,Py,Pz,fz,fy,fx)
         % Because the sqrt term in theta3 can be + or - we run through
         % all possible combinations (i = 2) and take the first combination that
         % satisfies the joint angle constraints.
+        sols = [];
         while nogo == 0;
             for i = 1:1:4
                 if i == 1
@@ -27,16 +28,19 @@ function [theta1,theta2,theta3,theta4,theta5,theta6] = invkin(Px,Py,Pz,fz,fy,fx)
                 a1 = 160;
                 a2 = 780;
                 a3 = 150;
+                d3 = 0;
                 d4 = 655;
                 rho = sqrt(Px^2+Py^2);
                 phi = atan2(Py,Px);
-                theta1 = atan2(Py,Px);
+                theta1 = (atan2(Py,Px)-atan2(d3,sign1*sqrt(Px^2+Py^2-d3^2)));
                 c1 = cos(theta1);
                 s1 = sin(theta1);
+                
                 K = (Px^2+Py^2+Pz^2+a1^2-(2*Px*a1)/c1-a2^2-a3^2-d4^2)/(2*a2);
                 theta3 = (atan2(a3,d4)-atan2(K,sign3*sqrt(a3^2+d4^2-K^2)));
                 c3 = cos(theta3);
                 s3 = sin(theta3);
+                
                 t23 = atan2((-a3-a2*c3)*Pz-(c1*Px+s1*Py-a1)*(d4-a2*s3),(a2*s3-d4)*Pz+(a3+a2*c3)*(c1*Px+s1*Py-a1));
                 theta2 = (t23 - theta3);
                 c2 = cos(theta2);
@@ -45,14 +49,13 @@ function [theta1,theta2,theta3,theta4,theta5,theta6] = invkin(Px,Py,Pz,fz,fy,fx)
                 R = eul2rotm([fz fy fx]);
                 s23 = ((-a3-a2*c3)*Pz+(c1*Px+s1*Py-a1)*(a2*s3-d4))/(Pz^2+(c1*Px+s1*Py-a1)^2);
                 c23 = ((a2*s3-d4)*Pz+(a3+a2*c3)*(c1*Px+s1*Py-a1))/(Pz^2+(c1*Px+s1*Py-a1)^2);
-                
                 r13 = R(1,3);
                 r23 = R(2,3);
                 r33 = R(3,3);
                 theta4 = atan2(-r13*s1+r23*c1,-r13*c1*c23-r23*s1*c23+r33*s23);
-
                 c4 = cos(theta4);
                 s4 = sin(theta4);
+                
                 r11 = R(1,1);
                 r21 = R(2,1);
                 r31 = R(3,1);
@@ -70,9 +73,10 @@ function [theta1,theta2,theta3,theta4,theta5,theta6] = invkin(Px,Py,Pz,fz,fy,fx)
                 theta4 = theta4*180/pi;
                 theta5 = theta5*180/pi;
                 theta6 = theta6*180/pi;
-%                 if theta2>=360-115 && theta2<=360-35
-%                     theta2 = 360-theta2;
-%                 end
+                sols = [sols;theta1 theta2 theta3 theta4 theta5 theta6;theta1 theta2 theta3 theta4+180 -theta5 theta6+180]; % Complete solution matrix
+                
+                % Solution check
+                % Last possible solution chosen
                 if theta1<=185 && theta1>=-185 && (theta2<=65 && theta2>=-185) && theta3<=175 && theta3>=-138 && theta4<=350 && theta4>=-350 && theta5<=130 && theta5>=-130 && theta6<=350 && theta6>=-350
                     nogo = 1;
                     break
